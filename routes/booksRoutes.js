9572245533d5da7a607364f3f4418c59005fd587
sbroad <sara.broad@aixplatform.com>
2019-11-24 20:50:54 -0500

    add working controllers

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -2,5 +2,10 @@ const router = require('express').Router();
 const booksController = require('../controllers/booksController');
 
 router.get('/books', booksController.getAllBooks)
+router.get('/books/:id', booksController.getBookById)
+router.post('/books', booksController.addBook)
+router.put('/books/:id', booksController.changeRating)
+router.delete('/books/:id', booksController.deleteBook)
+
 
 module.exports = router;
