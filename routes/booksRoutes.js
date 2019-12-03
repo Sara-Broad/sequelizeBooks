f098edfffa8e155030db43c80705b6d35b176674
sbroad <sara.broad@aixplatform.com>
2019-12-02 20:28:43 -0500

    tests

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-// const booksController = require('../controllers/booksController');
-const { getBookById } = require('../controllers/booksController');
+const booksController = require('../controllers/booksController');
 
-// router.get('/books', booksController.getAllBooks)
+router.get('/books', booksController.getAllBooks)
 router.get('/books/:id', getBookById)
-// router.post('/books', booksController.addBook)
-// router.put('/books/:id', booksController.changeRating)
-// router.delete('/books/:id', booksController.deleteBook)
+router.post('/books', booksController.addBook)
+router.put('/books/:id', booksController.changeRating)
+router.delete('/books/:id', booksController.deleteBook)
 
 
 module.exports = router;
