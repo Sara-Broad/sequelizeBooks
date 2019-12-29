177ce6f1c07bbb19fd3ebb3fb965740b802bfbdf
sbroad <sara.broad@aixplatform.com>
2019-12-28 20:35:20 -0500

    add working tests

--- test/routes.test.js
+++ test/routes.test.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 process.env.NODE_ENV = "test"
 const test = require('tape')
+const request = require('request');
 const db = require('../models')
 const {
-    // getBookById,
+    deleteBook,
     addBook,
     getAllBooks
 } = require('../controllers/booksController')
@@ -12,93 +13,120 @@ const httpMocks = require('node-mocks-http');
 
 
 
+// works but using Sinon
+// test('get all books resolves with a success message', (assert) => {
+//     const req = httpMocks.createRequest({
+//         method: 'GET',
+//         url: '/books',
+//     });
 
-test('get all books resolves with a success message', (assert) => {
-    const req = httpMocks.createRequest({
-        method: 'GET',
-        url: '/books',
-    });
+//     const res = httpMocks.createResponse();
 
-    const res = httpMocks.createResponse();
+//     getAllBooks(req, res).then((response) => {
+//         const data = response._getJSONData();
+//         assert.equal(typeof response.statusMessage, 'string');
+//         assert.ok(data.length > 1, 'There should be more than one entry')
+//         assert.equal('Boy Swallows Universe', data[0].title)
+//         assert.strictEqual(response.statusCode, 200, 'Expect 200 HTTP status code')
+//         assert.end()
+//     });
+// });
 
-    getAllBooks(req, res).then((response) => {
-        const data = response._getJSONData();
-        console.log(data.length)
-        assert.equal(typeof response.statusMessage, 'string');
-        assert.equal('Boy Swallows Universe', data[0].title)
-        assert.strictEqual(response.statusCode, 200, 'Expect 200 HTTP status code')
-        assert.end()
-    });
-});
-
-test.only('get all books resolves with a success message', (assert) => {
-    const bookMock = [
-        {
-            id: 1,
-            title: 'Heavy',
-            author: 'Kiese Laymon',
-            dateFinished: '2019-01-21',
-            pages: 256,
-            rating: 5,
-            createdAt: '2019-11-24T01:21:17.532Z',
-            updatedAt: '2019-11-24T01:21:17.532Z'
-          },
-          {
-            id: 2,
-            title: 'Boy Swallows Universe',
-            author: 'Trent Dalton',
-            dateFinished: '2019-04-27',
-            pages: 464,
-            rating: 5,
-            createdAt: '2019-11-24T01:21:17.532Z',
-            updatedAt: '2019-11-24T01:21:17.532Z'
-          }
-    ]
-    // const bookStub = sinon.stub().returns({})
-    // const req = {}
-    // const sendMock = sinon.spy()
-    // const res = {
-    //     send: sendMock
-    // }
-    
-    const req = {};
-    const res = {
-      status() {},
-      json() {}
+test('get all books should return a list of movies', (assert) => {
+ 
+    const responseObject = {
+        statusCode: 200,
+        headers: {
+            'content-type': 'application/json'
+        }
     };
+    const bookMock = {
+        status: 'success',
+        data: [{
+                id: 1,
+                title: 'Heavy',
+                author: 'Kiese Laymon',
+                dateFinished: '2019-01-21',
+                pages: 256,
+                rating: 5,
+                createdAt: '2019-11-24T01:21:17.532Z',
+                updatedAt: '2019-11-24T01:21:17.532Z'
+            },
+            {
+                id: 2,
+                title: 'Boy Swallows Universe',
+                author: 'Trent Dalton',
+                dateFinished: '2019-04-27',
+                pages: 464,
+                rating: 5,
+                createdAt: '2019-11-24T01:21:17.532Z',
+                updatedAt: '2019-11-24T01:21:17.532Z'
+            }
+        ]
+    }
 
-    // sinon.stub(res, 'json').returns({bookMock});
+    this.get = sinon.stub(request, 'get');
+    this.get.yields(null, responseObject, JSON.stringify(bookMock))
+    request.get('/books', (err, res, body) => {
+        // console.log(JSON.parse(body))
+        body = JSON.parse(body);
+        assert.equal(body.data.length, 2, 'there are two objects in the mock data')
+        assert.strictEqual(body.status, 'success', 'expect a success message')
+        assert.strictEqual(res.statusCode, 200, 'Expect 200 HTTP status code')
+        assert.end()
+    })
 
-
-
-    getAllBooks(req, res)
-    assert.ok(res.json.calledOnce, 'should be called once')
-    assert.end()
 });
 
 test('addBooks is called', (assert) => {
     const book = {
         addBook: (book) => {
-          this.book = book;
+            this.book = book;
         }
-      }
-    
-    const bookMock = [
-        {
-            "id": 2,
-            "title": "Boy Swallows Universe",
-            "author": "Trent Dalton",
-            "dateFinished": "2019-04-27T00:00:00.000Z",
-            "pages": 464,
-            "rating": 5,
-            "bookType": "fiction",
-            "createdAt": "2019-11-24T01:21:17.532Z",
-            "updatedAt": "2019-11-24T01:21:17.689Z"
-          }
-    ]
+    }
+
+    const bookMock = [{
+        "id": 2,
+        "title": "Boy Swallows Universe",
+        "author": "Trent Dalton",
+        "dateFinished": "2019-04-27T00:00:00.000Z",
+        "pages": 464,
+        "rating": 5,
+        "bookType": "fiction",
+        "createdAt": "2019-11-24T01:21:17.532Z",
+        "updatedAt": "2019-11-24T01:21:17.689Z"
+    }]
 
     sinon.spy(book, "addBook")
     book.addBook(bookMock)
     assert.ok(book.addBook.calledOnce, 'should be called once')
     assert.end()
+});
+
+test('Delete books is called and removes a book from the list', (assert) => {
+    const responseObject = {
+        statusCode: 200,
+        headers: {
+            'content-type': 'application/json'
+        }
+    };
+
+    const requestObject = {
+        status: 'success',
+        params: [
+            {
+                "id": 1
+            }
+        ]
+    }
+
+    this.delete = sinon.stub(request, 'delete')
+    this.delete.yields(null, responseObject, JSON.stringify(requestObject));
+    const bookId = requestObject.params[0].id
+
+    request.delete(`/books/${bookId}`, (err, res, body) => {
+        assert.strictEqual(responseObject.statusCode, 200, 'status code is a 200')
+        assert.strictEqual(requestObject.params[0].id, 1, `The request object has the bookId of ${bookId}`)
+        assert.end()
+    })
 });
